ff85d5f9ed9398c37411d968559f43201691ea81
Shlok Patil <codeshlok@gmail.com>
2019-09-07 08:34:13 -0400

    Added Character Anchor

--- Fight.py
+++ Fight.py
@@ -12,11 +12,12 @@ finished = False
 KeyPressed = pygame.key.get_pressed()
 Goku = 0
 
-gokuIdle = [pygame.image.load("Goku/4.png"),pygame.image.load("Goku/3.png")]
-gokuIT = [pygame.image.load("Goku/28.png"),pygame.image.load("Goku/29.png"), pygame.image.load("Goku/30.png"), pygame.image.load("Goku/31.png"), pygame.image.load("Goku/32.png"), pygame.image.load("Goku/33.png"), pygame.image.load("Goku/34.png"), pygame.image.load("Goku/35.png"), pygame.image.load("Goku/34.png"),pygame.image.load("Goku/33.png"), pygame.image.load("Goku/32.png"), pygame.image.load("Goku/31.png"), pygame.image.load("Goku/30.png"), pygame.image.load("Goku/28.png")]
+
+gokuIdle = [pygame.image.load("Goku/4.png"),pygame.image.load("Goku/5.png"), pygame.image.load("Goku/6.png")]
+gokuIT = [pygame.image.load("Goku/28.png"),pygame.image.load("Goku/29.png"), pygame.image.load("Goku/32.png"), pygame.image.load("Goku/33.png"), pygame.image.load("Goku/34.png"), pygame.image.load("Goku/35.png"), pygame.image.load("Goku/34.png"),pygame.image.load("Goku/33.png"), pygame.image.load("Goku/32.png"), pygame.image.load("Goku/28.png")]
 gokuCharge = [pygame.image.load("Goku/77.png"), pygame.image.load("Goku/78.png"), pygame.image.load("Goku/79.png")]
-uiIdle = [pygame.image.load("UIGoku/4.png"),pygame.image.load("UIGoku/5.png")]
-uiPunch = [pygame.image.load("UIGoku/13.png"),pygame.image.load("UIGoku/14.png")]
+uiIdle = [pygame.image.load("MUIGoku/7.png"),pygame.image.load("MUIGoku/8.png"), pygame.image.load("MUIGoku/9.png"), pygame.image.load("MUIGoku/10.png"), pygame.image.load("MUIGoku/11.png"), pygame.image.load("MUIGoku/12.png")]
+uiPunch = [pygame.image.load("MUIGoku/13.png"),pygame.image.load("MUIGoku/14.png")]
 left = False
 right = False
 idleCount = 0
@@ -32,40 +33,33 @@ while finished == False:
 
     screen.fill(white)
     KeyPressed = pygame.key.get_pressed()
-
+    if KeyPressed[pygame.K_i] == 1:
+        gCounter = 0
+        gStance = 1
     if KeyPressed[pygame.K_q] == 1:
         gStance = 2
     if KeyPressed[pygame.K_SPACE] == 1:
         gStance = 3
     if gStance == 1:
         Goku = gokuIdle
-        if gCounter < 1:
+        x = 450
+        y = 358
+        if gCounter < 2:
             gCounter = gCounter + 1
     if gStance == 2:
         Goku = gokuIT
         x = 450
         y = 358
-        if gCounter < 13:
+        if gCounter < 9:
             gCounter = gCounter + 1
     if gStance == 3:
         Goku = gokuCharge
-        x = 433
-        y = 340
+        x = 430
+        y = 338
         gCounter = gCounter + 1
         if gCounter > 2:
-            gCounter = 0
-    if gStance == 10:
-        Goku = uiIdle
-        gCounter = gCounter + 1
-        if gCounter > 1:
-            gCounter = 0
-
-    if gStance == 11:
-        Goku = uiPunch
-        gCounter = gCounter + 1
-        if gCounter > 1:
             gCounter = 0
 
     screen.blit(Goku[gCounter], (x, y))
     pygame.display.flip()
-    frame.tick(12)
+    frame.tick(13)
